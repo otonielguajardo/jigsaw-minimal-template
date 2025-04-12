27c990ff5d37f40e836bdbd5a2240332d189a0cb
Otoniel Guajardo <otoniel@medpal.mx>
2025-04-12 14:20:37 -0600

    Remove laravel mix

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -6,8 +6,8 @@
         <link rel="canonical" href="{{ $page->getUrl() }}">
         <meta name="description" content="{{ $page->description }}">
         <title>{{ $page->title }}</title>
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-        <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+        <link rel="stylesheet" href="{{ url('assets/css/main.css') }}">
+        <script defer src="{{ url('assets/js/main.js') }}"></script>
     </head>
     <body>
         @yield('body')
